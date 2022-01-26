bc0f9b1c544db5edd571ede4878861c562236d91
Grégori Mignerot <gregori.mignerot@cirad.fr>
2022-01-26 16:57:20 +0100

    Annotate and save variants, update header

--- src/fr/cirad/mgdb/annotation/SnpEffAnnotationService.java
+++ src/fr/cirad/mgdb/annotation/SnpEffAnnotationService.java
@@ -1,58 +1,92 @@
 package fr.cirad.mgdb.annotation;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.log4j.Logger;
+import org.bson.Document;
 import org.snpeff.interval.Chromosome;
 import org.snpeff.interval.Genome;
 import org.snpeff.snpEffect.Config;
 import org.snpeff.snpEffect.SnpEffectPredictor;
 import org.snpeff.snpEffect.VariantEffect;
-import org.snpeff.snpEffect.VariantEffect.EffectImpact;
 import org.snpeff.snpEffect.VariantEffects;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.FindIterable;
+
+import fr.cirad.mgdb.importing.VcfImport;
+import fr.cirad.mgdb.model.mongo.maintypes.DBVCFHeader;
+import fr.cirad.mgdb.model.mongo.maintypes.DBVCFHeader.VcfHeaderId;
 import fr.cirad.mgdb.model.mongo.maintypes.VariantRunData;
 import fr.cirad.tools.mongo.MongoTemplateManager;
+import htsjdk.variant.vcf.VCFHeaderLineCount;
+import htsjdk.variant.vcf.VCFHeaderLineType;
+import htsjdk.variant.vcf.VCFInfoHeaderLine;
 
 
 public class SnpEffAnnotationService {
-	public static String annotateProject(String module, int project, String snpEffDatabase) {
+    protected static final Logger LOG = Logger.getLogger(SnpEffAnnotationService.class);
+
+	public static String annotateRun(String module, int project, String run, String snpEffDatabase) {
 		MongoTemplate template = MongoTemplateManager.get(module);
 
+		LOG.info("Loading config");
 		Config config = new Config(snpEffDatabase, "/home/u017-h433/Documents/deps/snpEff/snpEff.config", "/home/u017-h433/Documents/deps/snpEff/data", null);
 		Genome genome = config.getGenome();
 		SnpEffectPredictor predictor = config.loadSnpEffectPredictor();
 		predictor.buildForest();
-		System.out.println("Loaded genome " + genome.getGenomeId());
+		LOG.info("Loaded genome " + genome.getGenomeId());
 
-		//BasicDBObject queryVarAnn = new BasicDBObject();
-        //queryVarAnn.put("_id." + DBVCFHeader.VcfHeaderId.FIELDNAME_PROJECT, project);
-        //DBVCFHeader headerData = DBVCFHeader.fromDocument(MongoTemplateManager.get(module).getCollection(MongoTemplateManager.getMongoCollectionName(DBVCFHeader.class)).find(queryVarAnn).first());
+		BasicDBObject vrdQuery = new BasicDBObject();
+		vrdQuery.put("_id." + VariantRunData.VariantRunDataId.FIELDNAME_PROJECT_ID, project);
+		vrdQuery.put("_id." + VariantRunData.VariantRunDataId.FIELDNAME_RUNNAME, run);
+		FindIterable<Document> variantRunData = template.getCollection(MongoTemplateManager.getMongoCollectionName(VariantRunData.class)).find(vrdQuery).batchSize(100);
 
-		Query vrdQuery = new Query();
-		vrdQuery.addCriteria(Criteria.where("_id." + VariantRunData.VariantRunDataId.FIELDNAME_PROJECT_ID).is(project));
-		List<VariantRunData> variantRunData = template.find(vrdQuery, VariantRunData.class);
-
-		for (VariantRunData vrd : variantRunData) {
+		LOG.info("Processing variants");
+		for (Document doc : variantRunData) {
+			VariantRunData vrd = template.getConverter().read(VariantRunData.class, doc);
 			Chromosome chromosome = getParentChromosome(vrd, genome);
-			System.out.println(chromosome.getChromosomeName() + " - " + vrd.getVariantId());
 			SnpEffVariantWrapper variant = new SnpEffVariantWrapper(chromosome, vrd, null);
-			annotateVariant(variant, predictor);
+
+			VariantRunData result = annotateVariant(variant, predictor);
+			if (result != null)
+				template.save(result);
 		}
+
+		LOG.info("Updating header");
+
+		BasicDBObject queryVarAnn = new BasicDBObject();
+        queryVarAnn.put("_id." + DBVCFHeader.VcfHeaderId.FIELDNAME_PROJECT, project);
+        queryVarAnn.put("_id." + DBVCFHeader.VcfHeaderId.FIELDNAME_RUN, run);
+
+        DBVCFHeader header = null;
+        Document headerDoc = template.getCollection(MongoTemplateManager.getMongoCollectionName(DBVCFHeader.class)).find(queryVarAnn).first();
+        if (headerDoc == null) {
+        	header = new DBVCFHeader(new VcfHeaderId(project, run), false, false, new HashMap<>(), new HashMap<>(), new HashMap<>(), new HashMap<>(), new HashMap<>());
+        } else {
+        	header = DBVCFHeader.fromDocument(headerDoc);
+        }
+
+        VCFInfoHeaderLine headerLine = new VCFInfoHeaderLine(VcfImport.ANNOTATION_FIELDNAME_ANN, VCFHeaderLineCount.UNBOUNDED, VCFHeaderLineType.String, "Predicted effects for this variant");
+        header.getmInfoMetaData().put(VcfImport.ANNOTATION_FIELDNAME_ANN, headerLine);
+
+        template.save(header);
 
 		return null;
 	}
 
-	private static boolean annotateVariant(SnpEffVariantWrapper variant, SnpEffectPredictor predictor) {
+	private static VariantRunData annotateVariant(SnpEffVariantWrapper variant, SnpEffectPredictor predictor) {
 		// Calculate effects: By default do not annotate non-variant sites
-		if (!variant.isVariant()) return false;
+		if (!variant.isVariant()) return null;
 
-		boolean impactModerateOrHigh = false; // Does this entry have a 'MODERATE' or 'HIGH' impact?
-		boolean impactLowOrHigher = false; // Does this entry have an impact (other than MODIFIER)?
+		/*boolean impactModerateOrHigh = false; // Does this entry have a 'MODERATE' or 'HIGH' impact?
+		boolean impactLowOrHigher = false; // Does this entry have an impact (other than MODIFIER)?*/
 
 		VariantEffects variantEffects = predictor.variantEffect(variant);
 
@@ -61,14 +95,12 @@ public class SnpEffAnnotationService {
 			/*if (variantEffect.hasError()) errByType.inc(variantEffect.getError());
 			if (variantEffect.hasWarning()) warnByType.inc(variantEffect.getWarning());*/
 
-			// Does this entry have an impact (other than MODIFIER)?
+			/*// Does this entry have an impact (other than MODIFIER)?
 			impactLowOrHigher |= (variantEffect.getEffectImpact() != EffectImpact.MODIFIER);
-			impactModerateOrHigh |= (variantEffect.getEffectImpact() == EffectImpact.MODERATE) || (variantEffect.getEffectImpact() == EffectImpact.HIGH);
+			impactModerateOrHigh |= (variantEffect.getEffectImpact() == EffectImpact.MODERATE) || (variantEffect.getEffectImpact() == EffectImpact.HIGH);*/
 			variant.addEffect(variantEffect);
 		}
-		System.out.println(impactLowOrHigher + " " + impactModerateOrHigh);
-
-		return impactLowOrHigher;
+		return variant.buildANN();
 	}
 
 	public static Chromosome getParentChromosome(VariantRunData vrd, Genome genome) {
--- src/fr/cirad/mgdb/annotation/SnpEffVariantWrapper.java
+++ src/fr/cirad/mgdb/annotation/SnpEffVariantWrapper.java
@@ -1,9 +1,16 @@
 package fr.cirad.mgdb.annotation;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import org.snpeff.interval.Marker;
 import org.snpeff.interval.Variant;
 import org.snpeff.snpEffect.VariantEffect;
+import org.snpeff.vcf.EffFormatVersion;
+import org.snpeff.vcf.VcfEffect;
 
+import fr.cirad.mgdb.importing.VcfImport;
 import fr.cirad.mgdb.model.mongo.maintypes.DBVCFHeader;
 import fr.cirad.mgdb.model.mongo.maintypes.VariantRunData;
 
@@ -11,13 +18,14 @@ import fr.cirad.mgdb.model.mongo.maintypes.VariantRunData;
 /**
  * Implementation of org.snpEff.interval.Variant's interface around an Mgdb2 variant
  * @author Grégori MIGNEROT
- * @implNote As this makes use of undocumented code that is not supposed to be used as a library, this deliberately contains lots of internal encapsulation to minimize the effort to keep it up to date, even if it harms performance a little bit
  */
 public class SnpEffVariantWrapper extends Variant {
 	private static final long serialVersionUID = 4989761070007311042L;
 
 	private VariantRunData variant;
 	private DBVCFHeader header;
+
+	private List<VariantEffect> effects;
 
 	public SnpEffVariantWrapper(Marker parent, VariantRunData vrd, DBVCFHeader header) {
 		super(parent,
@@ -28,9 +36,35 @@ public class SnpEffVariantWrapper extends Variant {
 
 		this.variant = vrd;
 		this.header = header;
+		this.effects = new ArrayList<VariantEffect>();
 	}
 
 	public void addEffect(VariantEffect effect) {
-		System.out.println(effect.toString());
+		this.effects.add(effect);
+	}
+
+	public VariantRunData buildANN() {
+		StringBuilder annotation = new StringBuilder();
+		ArrayList<String> effectNames = new ArrayList<>();
+		ArrayList<String> effectGenes = new ArrayList<>();
+
+		// FIXME : Some parts are missing or incomplete, check VcfEffect
+		for (VariantEffect effect : effects) {
+			VcfEffect vcfEffect = new VcfEffect(effect, EffFormatVersion.FORMAT_ANN);
+			annotation.append(vcfEffect.toString()).append(",");
+			effectNames.add(effect.getEffectTypeString(true));
+			if (effect.getGene() == null)
+				effectGenes.add("");
+			else
+				effectGenes.add(effect.getGene().getGeneName());
+		}
+		annotation.setLength(annotation.length() - 1);  // Trim the last comma
+
+		HashMap<String, Object> info = variant.getAdditionalInfo();
+		info.put(VcfImport.ANNOTATION_FIELDNAME_ANN, annotation.toString());
+		info.put(VariantRunData.FIELDNAME_ADDITIONAL_INFO_EFFECT_NAME, effectNames);
+		info.put(VariantRunData.FIELDNAME_ADDITIONAL_INFO_EFFECT_GENE, effectGenes);
+
+		return variant;
 	}
 }
