9b48edf48be077d7b3e6d4592724045371540095
sempere <guilhem.sempere@cirad.fr>
2023-05-24 19:22:21 +0200

    Accounting for end_site when creating indices (where applicable)

--- src/fr/cirad/mgdb/model/mongodao/MgdbDao.java
+++ src/fr/cirad/mgdb/model/mongodao/MgdbDao.java
@@ -71,6 +71,7 @@ import fr.cirad.mgdb.model.mongo.subtypes.ReferencePosition;
 import fr.cirad.tools.SessionAttributeAwareThread;
 import fr.cirad.tools.mongo.MongoTemplateManager;
 import fr.cirad.tools.security.base.AbstractTokenManager;
+import htsjdk.variant.variantcontext.VariantContext.Type;
 import htsjdk.variant.vcf.VCFConstants;
 import htsjdk.variant.vcf.VCFFormatHeaderLine;
 import htsjdk.variant.vcf.VCFHeaderLineCount;
@@ -230,6 +231,14 @@ public class MgdbDao {
             throw new Exception("An error occured while preparing database for searches, please check server logs");
         return result;
     }
+    
+    static public List<String> getVariantTypes(MongoTemplate mongoTemplate, Integer projId) {
+        Query q = new Query();
+        if (projId != null)
+        	q.addCriteria(Criteria.where("_id").is(projId));
+        List<String> res = mongoTemplate.findDistinct(q, GenotypingProject.FIELDNAME_VARIANT_TYPES, GenotypingProject.class, String.class);
+        return res;
+    }
 
     /**
      * Ensures position indexes are correct in passed collections. Supports
@@ -242,36 +251,48 @@ public class MgdbDao {
      */
     public static int ensurePositionIndexes(MongoTemplate mongoTemplate, Collection<MongoCollection<Document>> varColls) {
         int nResult = 0;
+        
+        List<String> variantTypes = getVariantTypes(mongoTemplate, null);
+        boolean fOnlySNPsInDB = variantTypes.size() == 1 && Type.SNP.toString().equals(variantTypes.iterator().next());
+        	
         List<Assembly> assemblies = mongoTemplate.findAll(Assembly.class);
         List<Integer> asmIDs = !assemblies.isEmpty() ? assemblies.stream().map(asm -> asm.getId()).collect(Collectors.toList()) : new ArrayList() {{ add(null); }};
         for (Integer assemblyId : asmIDs) {
 	        String rpPath = Assembly.getVariantRefPosPath(assemblyId) + ".";
-	        BasicDBObject coumpoundIndexKeys = new BasicDBObject(rpPath + ReferencePosition.FIELDNAME_SEQUENCE, 1).append(rpPath + ReferencePosition.FIELDNAME_START_SITE, 1), ssIndexKeys = new BasicDBObject(rpPath + ReferencePosition.FIELDNAME_START_SITE, 1);
+	        BasicDBObject startCompoundIndexKeys = new BasicDBObject(rpPath + ReferencePosition.FIELDNAME_SEQUENCE, 1).append(rpPath + ReferencePosition.FIELDNAME_START_SITE, 1), ssIndexKeys = new BasicDBObject(rpPath + ReferencePosition.FIELDNAME_START_SITE, 1);
+	        BasicDBObject endCompoundIndexKeys = new BasicDBObject(rpPath + ReferencePosition.FIELDNAME_SEQUENCE, 1).append(rpPath + ReferencePosition.FIELDNAME_END_SITE, 1), esIndexKeys = new BasicDBObject(rpPath + ReferencePosition.FIELDNAME_END_SITE, 1);
 	
 	        for (MongoCollection<Document> coll : varColls) {
 	            boolean fIsTmpColl = coll.getNamespace().getCollectionName().startsWith(MongoTemplateManager.TEMP_COLL_PREFIX);
-	            if (!fIsTmpColl && coll.estimatedDocumentCount() == 0) {
+	            if (!fIsTmpColl && coll.estimatedDocumentCount() == 0)
 	                continue;	// database seems empty: indexes will be created after imports (faster this way) 
-	            }
-	            boolean fFoundCoumpoundIndex = false, fFoundCorrectCoumpoundIndex = false, fFoundStartSiteIndex = false;
+
+	            boolean fFoundStartCompoundIndex = false, fFoundCorrectStartCompoundIndex = false, fFoundStartSiteIndex = false, fFoundEndCompoundIndex = false, fFoundCorrectEndCompoundIndex = false, fFoundEndSiteIndex = false;
 	            MongoCursor<Document> indexCursor = coll.listIndexes().cursor();
 	            while (indexCursor.hasNext()) {
 	                Document doc = (Document) indexCursor.next();
 	                Document keyDoc = ((Document) doc.get("key"));
 	                Set<String> keyIndex = (Set<String>) keyDoc.keySet();
 	                if (keyIndex.size() == 1) {
-	                    if ((rpPath + ReferencePosition.FIELDNAME_START_SITE).equals(keyIndex.iterator().next())) {
+	                    if ((rpPath + ReferencePosition.FIELDNAME_START_SITE).equals(keyIndex.iterator().next()))
 	                        fFoundStartSiteIndex = true;
-	                    }
+	                    else if (!fOnlySNPsInDB && (rpPath + ReferencePosition.FIELDNAME_END_SITE).equals(keyIndex.iterator().next()))
+	                    	fFoundEndSiteIndex = true;
 	                } else if (keyIndex.size() == 2) {	// compound index
 	                    String[] compoundIndexItems = keyIndex.toArray(new String[2]);
 	                    if (compoundIndexItems[0].equals(rpPath + ReferencePosition.FIELDNAME_SEQUENCE) && compoundIndexItems[1].equals(rpPath + ReferencePosition.FIELDNAME_START_SITE)) {
-	                        fFoundCoumpoundIndex = true;
+	                        fFoundStartCompoundIndex = true;
 	                        Document collation = (Document) doc.get("collation");
-	                        fFoundCorrectCoumpoundIndex = collation != null && "en_US".equals(collation.get("locale")) && Boolean.TRUE.equals(collation.get("numericOrdering"));
+	                        fFoundCorrectStartCompoundIndex = collation != null && "en_US".equals(collation.get("locale")) && Boolean.TRUE.equals(collation.get("numericOrdering"));
+	                    }
+	                    else if (!fOnlySNPsInDB && compoundIndexItems[0].equals(rpPath + ReferencePosition.FIELDNAME_SEQUENCE) && compoundIndexItems[1].equals(rpPath + ReferencePosition.FIELDNAME_END_SITE)) {
+	                    	fFoundEndCompoundIndex = true;
+	                        Document collation = (Document) doc.get("collation");
+	                        fFoundCorrectEndCompoundIndex = collation != null && "en_US".equals(collation.get("locale")) && Boolean.TRUE.equals(collation.get("numericOrdering"));
 	                    }
 	                }
 	            }
+
 	            if (!fFoundStartSiteIndex) {
 	                Thread ssIndexCreationThread = new Thread() {
 	                    public void run() {
@@ -282,24 +303,52 @@ public class MgdbDao {
 	                ssIndexCreationThread.start();
 	                nResult++;
 	            }
+	            
+	            if (!fOnlySNPsInDB && !fFoundEndSiteIndex) {
+	                Thread esIndexCreationThread = new Thread() {
+	                    public void run() {
+	                        LOG.log(fIsTmpColl ? Level.DEBUG : Level.INFO, "Creating index " + esIndexKeys + " on collection " + coll.getNamespace());
+	                        coll.createIndex(esIndexKeys);
+	                    }
+	                };
+	                esIndexCreationThread.start();
+	                nResult++;
+	            }
 	
-	            if (!fFoundCoumpoundIndex || (fFoundCoumpoundIndex && !fFoundCorrectCoumpoundIndex)) {
-	                final MongoCollection<Document> collToDropCompoundIndexOn = fFoundCoumpoundIndex ? coll : null;
+	            if (!fFoundStartCompoundIndex || (fFoundStartCompoundIndex && !fFoundCorrectStartCompoundIndex)) {
+	                final MongoCollection<Document> collToDropCompoundIndexOn = fFoundStartCompoundIndex ? coll : null;
 	                Thread ssIndexCreationThread = new Thread() {
 	                    public void run() {
 	                        if (collToDropCompoundIndexOn != null) {
-	                            LOG.log(fIsTmpColl ? Level.DEBUG : Level.INFO, "Dropping wrong index " + coumpoundIndexKeys + " on collection " + collToDropCompoundIndexOn.getNamespace());
-	                            collToDropCompoundIndexOn.dropIndex(coumpoundIndexKeys);
+	                            LOG.log(fIsTmpColl ? Level.DEBUG : Level.INFO, "Dropping wrong index " + startCompoundIndexKeys + " on collection " + collToDropCompoundIndexOn.getNamespace());
+	                            collToDropCompoundIndexOn.dropIndex(startCompoundIndexKeys);
 	                        }
 	
-	                        LOG.log(fIsTmpColl ? Level.DEBUG : Level.INFO, "Creating index " + coumpoundIndexKeys + " on collection " + coll.getNamespace());
-	                        coll.createIndex(coumpoundIndexKeys, new IndexOptions().collation(IExportHandler.collationObj));
+	                        LOG.log(fIsTmpColl ? Level.DEBUG : Level.INFO, "Creating index " + startCompoundIndexKeys + " on collection " + coll.getNamespace());
+	                        coll.createIndex(startCompoundIndexKeys, new IndexOptions().collation(IExportHandler.collationObj));
 	                    }
 	                };
 	                ssIndexCreationThread.start();
 	
 	                nResult++;
+	            }
+
+	            if (!fOnlySNPsInDB && (!fFoundEndCompoundIndex || (fFoundEndCompoundIndex && !fFoundCorrectEndCompoundIndex))) {
+	                final MongoCollection<Document> collToDropCompoundIndexOn = fFoundEndCompoundIndex ? coll : null;
+	                Thread esIndexCreationThread = new Thread() {
+	                    public void run() {
+	                        if (collToDropCompoundIndexOn != null) {
+	                            LOG.log(fIsTmpColl ? Level.DEBUG : Level.INFO, "Dropping wrong index " + endCompoundIndexKeys + " on collection " + collToDropCompoundIndexOn.getNamespace());
+	                            collToDropCompoundIndexOn.dropIndex(endCompoundIndexKeys);
+	                        }
 	
+	                        LOG.log(fIsTmpColl ? Level.DEBUG : Level.INFO, "Creating index " + endCompoundIndexKeys + " on collection " + coll.getNamespace());
+	                        coll.createIndex(endCompoundIndexKeys, new IndexOptions().collation(IExportHandler.collationObj));
+	                    }
+	                };
+	                esIndexCreationThread.start();
+	
+	                nResult++;
 	            }
 	        }
         }
