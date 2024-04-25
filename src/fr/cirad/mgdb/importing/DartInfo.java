a1fe48bd34929dd6c28fe32e8755e822e425e8cd
Dorian Grasset <dorian.grasset@cirad.fr>
2024-04-25 16:10:16 +0200

    update DartInfo class

--- src/fr/cirad/mgdb/importing/DartInfo.java
+++ src/fr/cirad/mgdb/importing/DartInfo.java
@@ -21,6 +21,14 @@ public class DartInfo {
 
     private String strand = "+";
 
+    private float callRate;
+
+    private float freqHomRef;
+
+    private float freqHomSnp;
+
+    private float freqHets;
+
     private String[] genotypes = null;
 
     private String[] alleles = new String[2];
@@ -32,62 +40,18 @@ public class DartInfo {
 //        this.individualName = individualName;
     }
 
-    public DartInfo(String alleleID, String alleleSequence, String trimmedSequence, String chrom, Integer chromPos, String individualName) {
+    public DartInfo(String alleleID, String alleleSequence, String trimmedSequence, String chrom, Integer chromPos, String individualName, float callRate, float freqHomRef, float freqHets) {
         this.alleleID = alleleID;
         this.alleleSequence = alleleSequence;
         this.trimmedSequence = trimmedSequence;
         this.chrom = chrom;
         this.chromPos = chromPos;
 //        this.individualName = individualName;
+        this.callRate = callRate;
+        this.freqHomRef = freqHomRef;
+        this.freqHets = freqHets;
     }
 
-    public String[] getSampleIDs() {
-        return sampleIDs;
-    }
-
-    public void setSampleIDs(String[] sampleIDs) {
-        this.sampleIDs = sampleIDs;
-    }
-
-    public String[] getAlleles() {
-        return alleles;
-    }
-
-    public void setAlleles(String[] alleles) {
-        this.alleles = alleles;
-    }
-
-    public String[] getGenotypes() {
-        return genotypes;
-    }
-
-    public void setGenotypes(String[] genotypes) {
-        this.genotypes = genotypes;
-    }
-
-    public Integer getChromPos() {
-        return chromPos;
-    }
-
-    public void setChromPos(Integer chromPos) {
-        this.chromPos = chromPos;
-    }
-
-    public String getStrand() {
-        return strand;
-    }
-
-    public void setStrand(String strand) {
-        this.strand = strand;
-    }
-//
-//    public String getIndividualName() {
-//        return individualName;
-//    }
-//
-//    public void setIndividualName(String individualName) {
-//        this.individualName = individualName;
-//    }
 
     public String getAlleleID() {
         return alleleID;
@@ -97,6 +61,7 @@ public class DartInfo {
         this.alleleID = alleleID;
     }
 
+
     public String getAlleleSequence() {
         return alleleSequence;
     }
@@ -104,6 +69,16 @@ public class DartInfo {
     public void setAlleleSequence(String alleleSequence) {
         this.alleleSequence = alleleSequence;
     }
+
+
+    public String getTrimmedSequence() {
+        return trimmedSequence;
+    }
+
+    public void setTrimmedSequence(String trimmedSequence) {
+        this.trimmedSequence = trimmedSequence;
+    }
+
 
     public String getChrom() {
         return chrom;
@@ -117,13 +92,23 @@ public class DartInfo {
         this.chrom = chrom;
     }
 
-    public String getTrimmedSequence() {
-        return trimmedSequence;
+
+    public Integer getChromPos() {
+        return chromPos;
     }
 
-    public void setTrimmedSequence(String trimmedSequence) {
-        this.trimmedSequence = trimmedSequence;
+    public void setChromPos(Integer chromPos) {
+        this.chromPos = chromPos;
     }
+
+
+//    public String getIndividualName() {
+//        return individualName;
+//    }
+//
+//    public void setIndividualName(String individualName) {
+//        this.individualName = individualName;
+//    }
 
     public Integer getSnpPos() {
         return snpPos;
@@ -132,6 +117,78 @@ public class DartInfo {
     public void setSnpPos(Integer snpPos) {
         this.snpPos = snpPos;
     }
+
+
+    public String getStrand() {
+        return strand;
+    }
+
+    public void setStrand(String strand) {
+        this.strand = strand;
+    }
+
+
+    public float getCallRate() {
+        return callRate;
+    }
+
+    public void setCallRate(float callRate) {
+        this.callRate = callRate;
+    }
+
+
+    public float getFreqHomRef() {
+        return freqHomRef;
+    }
+
+    public void setFreqHomRef(float freqHomRef) {
+        this.freqHomRef = freqHomRef;
+    }
+
+
+    public float getFreqHomSnp() {
+        return freqHomSnp;
+    }
+
+    public void setFreqHomSnp(float freqHomSnp) {
+        this.freqHomSnp = freqHomSnp;
+    }
+
+
+    public float getFreqHets() {
+        return freqHets;
+    }
+
+    public void setFreqHets(float freqHets) {
+        this.freqHets = freqHets;
+    }
+
+    public String[] getGenotypes() {
+        return genotypes;
+    }
+
+    public void setGenotypes(String[] genotypes) {
+        this.genotypes = genotypes;
+    }
+
+
+    public String[] getAlleles() {
+        return alleles;
+    }
+
+    public void setAlleles(String[] alleles) {
+        this.alleles = alleles;
+    }
+
+
+    public String[] getSampleIDs() {
+        return sampleIDs;
+    }
+
+    public void setSampleIDs(String[] sampleIDs) {
+        this.sampleIDs = sampleIDs;
+    }
+
 
     public Integer variantPos() {
         if (strand.equals("-") || strand.equals("Minus")) {
