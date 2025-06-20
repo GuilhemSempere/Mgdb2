cb2b4206c3e36fc8ba1c2bf1a7c4ab64346899ce
GuilhemSempere <guilhem.sempere@cirad.fr>
2025-06-20 16:01:33 +0200

    Staging (#86)
    * Merge from master (#82)
    
    * Merged from staging for next release (#80)
    
    * Added means to import INDELs from PLINK data, alleles encoded as I and D
    
    ... the workaround to make it htsjdk compatible is turning I into NN and
    D in to N
    
    * Generalizing support for I and D alleles
    
    * add function to check if the user can edit calls
    
    * Exporting alleles of INDELs with unknown sequence as I/D
    
    for all individual-oriented formats
    
    * Export optim (#78)
    
    * Implemented async exports from temp collections
    
    * POM dependency upgrade
    
    * Parallelized export functional for variant-oriented formats
    
    * Added means to write variant-list files as chunks when exporting
    
    * Minor changes
    
    * Individual-oriented exports now working in parallel
    
    * Minor change
    
    * Added mean to specify a location for export extraction files
    
    * Added mean to specify a location for export extraction files
    
    + fixed outputstream flush / close
    
    * All seems to be working fine
    
    * Minor changes
    
    * insertStep method in ProgressIndicator adds step at next position
    
    * Added safelyGetKnownAlleles() method
    
    which allows to fix incomplete known allele lists in VRD records
    
    * Replacing dots with underscores in metadata field names
    
    This used to cause trouble in the JSON document structure
    
    * Made taxon update immediately accounted for
    
    * add listReadableDbs in AbstractTokenManager
    
    * POM version update
    
    * Filter export data on project IDs if several projects in DB
    
    (faster export!)
    
    * Added some debug facility to GroupedExecutor
    
    * Improved debug info for GroupedExecutor
    
    * Minor change
    
    * Fixed chunk count problem with IGV exports
    
    * Minor changes
    
    * Minor changes
    
    * Moved VisualizationService class from Gigwa2ServiceImpl to Mgdb2Export
    
    * Made readToken method static
    
    ---------
    
    Co-authored-by: Alice Boizet <alice.boizet@cirad.fr>
    
    * Provided means to generate prefixed variant IDs at initial import time
    
    * Reusing known INDEL alleles instead of adding N/NN
    
    ... for cases where we work on a predefined biallelic variant list
    
    * Added support for Affymetrix variant IDs
    
    * getSamplesByIndividualForProject now accounts for empty individual list
    
    * Renamed Affymetrix synonym type to Axiom
    
    * Added support for databaseNamePrefix config property
    
    * Made identification of variants by position optional
    
    * Remove project refs from variants collection when deleting projects
    
    * Fixed sporadic bug in importing individuals when sample names provided
    
    ---------
    
    Co-authored-by: Alice Boizet <alice.boizet@cirad.fr>
    
    * Forbid duplicate IDs in metadata
    
    * Tiny fix
    
    * Reverted accidental commit
    
    ---------
    
    Co-authored-by: Alice Boizet <alice.boizet@cirad.fr>

--- src/fr/cirad/mgdb/importing/IndividualMetadataImport.java
+++ src/fr/cirad/mgdb/importing/IndividualMetadataImport.java
@@ -233,8 +233,8 @@ public class IndividualMetadataImport {
                 nProcessedEntityCount++;
             }
 
-            if (targetEntityList.size() != new HashSet<>(targetEntityList).size())
-            	throw new Exception("The provided metadata contains duplicate " + targetTypeColName + "s");
+            if (targetEntityList.size() != new java.util.HashSet<>(targetEntityList).size())
+                throw new Exception("The provided metadata contains duplicate " + targetTypeColName + "s");
             else if (targetEntityList.size() == 0) { // a blank metadata file was submitted: let's delete any existing metadata
                 if (username == null) { // global metadata
                     bulkOperations.updateMulti(new Query(), new Update().unset(targetTypeColName.equals("sample") ? GenotypingSample.SECTION_ADDITIONAL_INFO : Individual.SECTION_ADDITIONAL_INFO));				
